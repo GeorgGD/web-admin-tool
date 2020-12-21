952018ff82fb6b87206a3ac17c8ac02fc45813c7
GeorgGD <giorgos_davak@hotmail.com>
2020-12-21 23:55:11 +0100

    Make mapping work for post request

--- src/main/java/com/webAdminTool/controllers/LoginAuth.java
+++ src/main/java/com/webAdminTool/controllers/LoginAuth.java
@@ -2,11 +2,12 @@ package com.webAdminTool.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginAuth {
 
-	@RequestMapping("/login")
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login() {
 		return "dashboard";
 	}
