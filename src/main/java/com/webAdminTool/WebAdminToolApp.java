27216c90705ccdb07f25052e236237dc600870f3
GeorgGD <giorgos_davak@hotmail.com>
2020-12-21 16:00:36 +0100

    Revert changes

--- src/main/java/com/webAdminTool/WebAdminToolApp.java
+++ src/main/java/com/webAdminTool/WebAdminToolApp.java
@@ -2,17 +2,10 @@ package com.webAdminTool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebAdminToolApp extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(WebAdminToolApp.class);
-    }
-    
+public class WebAdminToolApp {
+   
 	public static void main(String[] args) {
 		SpringApplication.run(WebAdminToolApp.class, args);
 	}
