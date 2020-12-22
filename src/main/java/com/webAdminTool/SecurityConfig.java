ef1ecda44e71d9836dc1dac5257b2e5caa0407cc
GeorgGD <giorgos_davak@hotmail.com>
2020-12-22 21:12:18 +0100

    Introduce SecurityConfig

---
+++ src/main/java/com/webAdminTool/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.webAdminTool;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user")
+			.password("password")
+			.roles("admin");
+	}
+}
