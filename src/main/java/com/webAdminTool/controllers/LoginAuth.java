ebf4c34162b3cfed193e282569a24dc1412e620a
GeorgGD <giorgos_davak@hotmail.com>
2020-12-24 16:49:52 +0100

    Remove unused code

--- src/main/java/com/webAdminTool/controllers/LoginAuth.java
+++ src/main/java/com/webAdminTool/controllers/LoginAuth.java
@@ -2,24 +2,12 @@ package com.webAdminTool.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginAuth {
-
-/*	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String redirect() {
-		return "dashboard";
-	}*/
 	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
-	}
-	
-	@GetMapping("/test")
-	public String test() {
-		return "dashboard";
-	}
+	}    
 }
