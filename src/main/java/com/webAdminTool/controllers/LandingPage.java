a11edfadf43789cc84c2e47dabb9b4106825e8dc
GeorgGD <giorgos_davak@hotmail.com>
2020-12-24 17:14:51 +0100

    Add documentation

--- src/main/java/com/webAdminTool/controllers/LandingPage.java
+++ src/main/java/com/webAdminTool/controllers/LandingPage.java
@@ -3,10 +3,15 @@ package com.webAdminTool.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * The following controller handles redirection from the dashboard meny
+ * @author Georgios Davakos
+ * @since 2020-12-24
+ */
 @Controller
 public class LandingPage {
 	
-	@GetMapping("/")
+	@GetMapping("/dashboard")
 	public String index() {
 		return "dashboard";
 	}
