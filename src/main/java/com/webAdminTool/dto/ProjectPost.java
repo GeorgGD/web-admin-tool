01628dcee9ea6402966383b53a05c18b1536e047
GeorgGD <giorgos_davak@hotmail.com>
2021-01-01 19:24:44 +0100

    Introduce ProjectPost

---
+++ src/main/java/com/webAdminTool/dto/ProjectPost.java
@@ -0,0 +1,10 @@
+package com.webAdminTool.dto;
+
+public class ProjectPost {
+
+	private int ID;
+	private String title;
+	private String short_desc;
+	private String post;
+	private String date;
+}
