1b244207674805616bfe30ea28acff79a3143cc0
GeorgGD <giorgos_davak@hotmail.com>
2021-01-01 19:27:10 +0100

    Add getters and setters

--- src/main/java/com/webAdminTool/dto/ProjectPost.java
+++ src/main/java/com/webAdminTool/dto/ProjectPost.java
@@ -7,4 +7,46 @@ public class ProjectPost {
 	private String short_desc;
 	private String post;
 	private String date;
+
+	public int getID() {
+		return ID;
+	}
+	
+	public void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getShort_desc() {
+		return short_desc;
+	}
+
+	public void setShort_desc(String short_desc) {
+		this.short_desc = short_desc;
+	}
+
+	public String getPost() {
+		return post;
+	}
+
+	public void setPost(String post) {
+		this.post = post;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	
 }
