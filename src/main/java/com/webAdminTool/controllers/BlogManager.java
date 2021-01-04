c423b257b03fb19b955a3c66fce35796f4b97a9c
GeorgGD <giorgos_davak@hotmail.com>
2021-01-04 01:46:15 +0100

    Introduce blogManagerPanel

--- src/main/java/com/webAdminTool/controllers/BlogManager.java
+++ src/main/java/com/webAdminTool/controllers/BlogManager.java
@@ -1,9 +1,27 @@
 package com.webAdminTool.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.webAdminTool.dto.BlogPost;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin/dashboard")
 public class BlogManager {
 
 	// Some JDBC handler variable
+	@RequestMapping("/blog")
+	public String blogManagerPanel(Model model) {
+		// Call server for blog post entries
+
+		// Create a table with all the entries
+		List<BlogPost> blogEntries = new ArrayList<>();
+		String table = createTable(blogEntries);
+		model.addAttribute("table", table);
+		return "blogManager";
+	}
 }
