465f7a5a1a20cfcd3e79f9709a521d8024cd77e4
GeorgGD <giorgos_davak@hotmail.com>
2021-01-04 01:32:49 +0100

    Add comments

--- src/main/java/com/webAdminTool/dto/blogPost.java
+++ src/main/java/com/webAdminTool/dto/blogPost.java
@@ -48,6 +48,7 @@ public class blogPost {
 		this.date = date;
 	}
 
+	// Creates a row for a table entry
 	public String toString() {
 		return "<tr>"+"<td>"+ID+"</td><td>"+title+"</td><td>"+date+"</td><td class=\"text-center\"><a href=\"edit?id="+ID+"\"><i class=\"fas fa-info-circle text-orange\"></i></a></td>";
 	}		
