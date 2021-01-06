bcd651aec7d9c00f3162f5182b5755a60a81aef6
GeorgGD <giorgos_davak@hotmail.com>
2021-01-06 23:24:41 +0100

    Intoduce getAllPosts

--- src/main/java/com/webAdminTool/service/BlogService.java
+++ src/main/java/com/webAdminTool/service/BlogService.java
@@ -1,8 +1,23 @@
 package com.webAdminTool.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.webAdminTool.dto.BlogPost;
+import com.webAdminTool.repository.BlogRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BlogService {
-	
+
+	@Autowired
+	private BlogRepository blogRepo;
+
+	public List<BlogPost> getAllPosts() {
+		List<BlogPost> posts = new ArrayList<>();
+		blogRepo.findAll().forEach(posts::add);
+		return posts;
+	}
 }
