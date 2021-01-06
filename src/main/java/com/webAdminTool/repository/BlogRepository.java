636009ca3de4929265bd696ff07878a70885e47a
GeorgGD <giorgos_davak@hotmail.com>
2021-01-06 23:02:35 +0100

    Introduce Component annotation

--- src/main/java/com/webAdminTool/repository/BlogRepository.java
+++ src/main/java/com/webAdminTool/repository/BlogRepository.java
@@ -3,7 +3,9 @@ package com.webAdminTool.repository;
 import com.webAdminTool.dto.BlogPost;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
+@Component
 public interface BlogRepository extends CrudRepository <BlogPost, Integer>{
 	
 }
