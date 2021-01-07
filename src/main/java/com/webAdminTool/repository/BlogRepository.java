3390c0e171d6e91ac12fe5b09ba091885c400577
GeorgGD <giorgos_davak@hotmail.com>
2021-01-07 17:05:02 +0100

    Add docs

--- src/main/java/com/webAdminTool/repository/BlogRepository.java
+++ src/main/java/com/webAdminTool/repository/BlogRepository.java
@@ -5,6 +5,9 @@ import com.webAdminTool.dto.BlogPost;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+/**
+ * Meant for connecting to external database 
+ */
 @Repository
 public interface BlogRepository extends CrudRepository <BlogPost, Integer>{
 	
