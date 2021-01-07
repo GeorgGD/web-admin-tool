0cbe0422fe8697e33ec865df472168c0aa5a1f10
GeorgGD <giorgos_davak@hotmail.com>
2021-01-07 17:08:34 +0100

    Add more docs

--- src/main/java/com/webAdminTool/repository/BlogRepository.java
+++ src/main/java/com/webAdminTool/repository/BlogRepository.java
@@ -6,7 +6,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 /**
- * Meant for connecting to external database 
+ * Meant for connecting to external database
+ * @author Georgios Davakos
+ * @since 2021-01-07 
  */
 @Repository
 public interface BlogRepository extends CrudRepository <BlogPost, Integer>{
