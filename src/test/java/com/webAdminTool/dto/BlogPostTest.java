9a3e2f9ca6a21c3223f43221d4498efd8c769bba
GeorgGD <giorgos_davak@hotmail.com>
2021-01-12 19:05:13 +0100

    Introduce getSetIDTest

--- src/test/java/com/webAdminTool/dto/BlogPostTest.java
+++ src/test/java/com/webAdminTool/dto/BlogPostTest.java
@@ -1,11 +1,19 @@
 package com.webAdminTool.dto;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
 public class BlogPostTest {
 
 	@Test
-	public void getIdDTest() {
-		
+	public void getSetIdDTest() {
+		int id = 10;
+		BlogPost blog = new BlogPost();
+
+		blog.setID(id);
+		int actualId = blog.getID();
+
+		assertEquals(id, actualId);
 	}
 }
