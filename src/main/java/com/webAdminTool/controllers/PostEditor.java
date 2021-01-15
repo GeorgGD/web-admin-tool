7432f36760d4ce2944b2de9bc7b0b09916946267
GeorgGD <giorgos_davak@hotmail.com>
2021-01-15 20:16:51 +0100

    Introduce createPost

--- src/main/java/com/webAdminTool/controllers/PostEditor.java
+++ src/main/java/com/webAdminTool/controllers/PostEditor.java
@@ -1,19 +1,34 @@
 package com.webAdminTool.controllers;
 
+import com.webAdminTool.dto.BlogPost;
 import com.webAdminTool.service.BlogService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/dashboard")
+@RequestMapping("/blogManager")
 public class PostEditor {
 
 	// Some JPA handler variable
 	@Autowired
 	BlogService blogService;
 
+	/**
+	 * Redirects to the blog editor
+	 * @param blogPost The blog post
+	 * @return The view
+	 */
+	@RequestMapping("/addBlogPost")
+	public String addBlogPost(@ModelAttribute("postData") BlogPost blogPost) {
+		return "postEdit";
+	}
+	
+	/**
+	 * Creates a 
+	 */
 	@RequestMapping("/createPost")
 	public String createPost() {
 
