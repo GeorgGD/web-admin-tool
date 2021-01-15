e4ef3b5ae0ece7b4dd29d9d9ba4a3985fc15085c
GeorgGD <giorgos_davak@hotmail.com>
2021-01-15 21:10:24 +0100

    Change the implementation of addBlogPost

--- src/main/java/com/webAdminTool/controllers/PostEditor.java
+++ src/main/java/com/webAdminTool/controllers/PostEditor.java
@@ -29,7 +29,7 @@ public class PostEditor {
 	 */
 	@RequestMapping("/addBlogPost")
 	public String addBlogPost(@ModelAttribute("postData") BlogPost blogPost) {
-					
+		blogPost.setID(nextAvailableID());
 		return "postEdit";
 	}
 	
