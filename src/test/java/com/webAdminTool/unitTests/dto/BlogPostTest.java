cf5d5d66b524cbbf20c729d4224614bb197e329b
GeorgGD <giorgos_davak@hotmail.com>
2021-01-18 21:57:54 +0100

    Fix package and imports

--- src/test/java/com/webAdminTool/unitTests/dto/BlogPostTest.java
+++ src/test/java/com/webAdminTool/unitTests/dto/BlogPostTest.java
@@ -1,6 +1,8 @@
-package com.webAdminTool.dto;
+package com.webAdminTool.unitTests.dto;
 
 import static org.junit.Assert.assertEquals;
+
+import com.webAdminTool.dto.BlogPost;
 
 import org.junit.Test;
 
