480749d79acbd62d2b91933fd22b7d2144d444eb
GeorgGD <giorgos_davak@hotmail.com>
2021-01-18 22:07:32 +0100

    Introduce initPost

--- src/test/java/com/webAdminTool/unitTests/service/BlogServiceTest.java
+++ src/test/java/com/webAdminTool/unitTests/service/BlogServiceTest.java
@@ -1,11 +1,24 @@
 package com.webAdminTool.unitTests.service;
 
+import com.webAdminTool.dto.BlogPost;
 import com.webAdminTool.service.BlogService;
 
 import org.junit.Test;
 
 public class BlogServiceTest {
 
+
+	public BlogPost initPost() {
+		BlogPost post = new BlogPost();
+		post.setID(1);
+		post.setTitle("test");
+		post.setShort_desc("A unit test");
+		post.setPost("a short unit test");
+		post.setPost_date("21/01/18");
+
+		return post;
+	}
+	
 	@Test
 	public void createBlogPostTest() {
 		
