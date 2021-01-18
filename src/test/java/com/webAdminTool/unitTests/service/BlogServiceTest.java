733476ed48edfe8f4d7c097227a639868f09a794
GeorgGD <giorgos_davak@hotmail.com>
2021-01-18 22:03:54 +0100

    Introduce BlogServiceTest

---
+++ src/test/java/com/webAdminTool/unitTests/service/BlogServiceTest.java
@@ -0,0 +1,13 @@
+package com.webAdminTool.unitTests.service;
+
+import com.webAdminTool.service.BlogService;
+
+import org.junit.Test;
+
+public class BlogServiceTest {
+
+	@Test
+	public void createBlogPostTest() {
+		
+	}
+}
