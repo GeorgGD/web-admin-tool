4594c3f24d294bbf4a6b43f0099ae741da7f451e
GeorgGD <giorgos_davak@hotmail.com>
2021-01-19 18:37:33 +0100

    Introduce setup

--- src/test/java/com/webAdminTool/controllers/BlogManagerTest.java
+++ src/test/java/com/webAdminTool/controllers/BlogManagerTest.java
@@ -1,9 +1,15 @@
 package com.webAdminTool.controllers;
 
+import java.util.HashMap;
+
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.MapBindingResult;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,4 +18,12 @@ public class BlogManagerTest {
 	@Autowired
 	private BlogManager blogManager;
 
+	private Model model;
+	private MapBindingResult errors;
+	
+	@Before
+	public void setup() {
+		model = new ExtendedModelMap();
+		errors = new MapBindingResult(new HashMap<String, String>(), "dummy");
+	}
 }
