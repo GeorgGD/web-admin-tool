13de45cffa4f35b6f24ea146ab417af41fa7eed3
GeorgGD <giorgos_davak@hotmail.com>
2021-01-20 21:24:59 +0100

    Introduce ProjectPostEditorTest

---
+++ src/test/java/com/webAdminTool/controllers/ProjectPostEditorTest.java
@@ -0,0 +1,26 @@
+package com.webAdminTool.controllers;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashMap;
+
+import com.webAdminTool.dto.BlogPost;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.MapBindingResult;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@FixMethodOrder(MethodSorters.DEFAULT)
+public class ProjectPostEditorTest {
+	
+}
