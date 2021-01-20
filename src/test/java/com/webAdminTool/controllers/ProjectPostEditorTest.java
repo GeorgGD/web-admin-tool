e2d70b53bf125c90597008a8619066e2982e96d2
GeorgGD <giorgos_davak@hotmail.com>
2021-01-20 21:30:11 +0100

    Implement ProjectPostEditorTest

--- src/test/java/com/webAdminTool/controllers/ProjectPostEditorTest.java
+++ src/test/java/com/webAdminTool/controllers/ProjectPostEditorTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.HashMap;
 
-import com.webAdminTool.dto.BlogPost;
+import com.webAdminTool.dto.ProjectPost;
 
 import org.junit.Before;
 import org.junit.FixMethodOrder;
@@ -22,5 +22,57 @@ import org.springframework.validation.MapBindingResult;
 @SpringBootTest
 @FixMethodOrder(MethodSorters.DEFAULT)
 public class ProjectPostEditorTest {
+
+	@Autowired
+	private ProjectPostEditor postEditor;
+	private Model model;
+	private MapBindingResult errors;
+	private ProjectPost post;
 	
+	@Before
+	public void setup() {
+		model = new ExtendedModelMap();
+		errors = new MapBindingResult(new HashMap<String, String>(), "dummy");
+
+		post = new ProjectPost();
+		post.setID(1);
+		post.setTitle("test");
+		post.setShort_desc("A unit test");
+		post.setPost("a short unit test");
+		post.setPost_date("21/01/18");
+	}
+
+	@Test
+	public void rootRedirectTest() {
+		String expectedString = "redirect:/dashboard/projectManager";
+		String actualString = postEditor.rootRedirect();
+
+		assertEquals(expectedString, actualString);
+	}
+
+	@Test
+	public void addProjectPostTest() {
+		String expectedView = "postEdit";
+		String actualView = postEditor.addProjectPost(post);
+		assertEquals(expectedView, actualView);
+	}
+
+	@Test
+	public void createPostTest() {
+		String expectedView = "redirect:/dashboard/projectManager";		
+		String actualView = postEditor.createPost(post, errors);
+
+		assertEquals(expectedView, actualView);
+		postEditor.deletePost(1);
+	}
+
+	@Test
+	public void editPostTest() {
+		postEditor.createPost(post, errors);
+		String expectedView = "postEdit";
+		String actualView = postEditor.editPost(post, 1, model);
+
+		assertEquals(expectedView, actualView);
+		postEditor.deletePost(1);
+	}
 }
